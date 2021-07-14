fedd1ce230a196dbc6a1ed7770e8c507b651f07f
Killian Farrell <killianfarrell2@hotmail.com>
2021-07-14 22:11:30 +0100

    Changed column order

--- Tournament_level_model/Data_manipulation/Import_data_cleanse.py
+++ Tournament_level_model/Data_manipulation/Import_data_cleanse.py
@@ -7,9 +7,9 @@ raw_data_location = 'D:\\KF_Repo\\PGA_Golf\\Tournament_level_model\\Raw_data\\AS
 tournament = pd.read_csv(raw_data_location)
 
 #Select fewer columns
-reduce_columns = tournament[['Player_initial_last', 'tournament id', 'player id', 'hole_par',
+reduce_columns = tournament[['player', 'player id', 'hole_par',
        'strokes', 'n_rounds', 'made_cut', 
-       'player', 'tournament name', 'course', 'date',
+       'tournament name','tournament id', 'course', 'date',
        'purse', 'season', 'no_cut', 'Finish', 'sg_putt', 'sg_arg', 'sg_app',
        'sg_ott', 'sg_t2g', 'sg_total']]
 
