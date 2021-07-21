3180236fd782a1900bfd4a4eabd5385e15a42162
Killian Farrell <killianfarrell2@hotmail.com>
2021-07-21 22:02:52 +0100

    Update Explore_subset_data.py

--- Tournament_level_model/Data_exploration/Explore_subset_data.py
+++ Tournament_level_model/Data_exploration/Explore_subset_data.py
@@ -22,12 +22,43 @@ hole_level_data = pd.read_csv(hole_level_location)
 #Additional
 #Stokes gained ball striking = sg ott + sg app
 
-#Filter data for 2021 US Open
-us_open_2021 = subset_data[subset_data['tournament id']==401243414]
+#Filter data for 2020 PGA 
+us_pga_2020 = tournament_data[tournament_data['tournament id']==401219481]
+us_pga_2020['round_par'] = us_pga_2020['hole_par']/us_pga_2020['n_rounds']
+us_pga_2020['Final_score'] = us_pga_2020['strokes'] - us_pga_2020['hole_par']
 
-#Compare Strokes
-compare = us_open_2021[['sg_total','hole_par','strokes','made_cut']]
-compare['score'] = compare['strokes'] - compare['hole_par']
+
+#Filter data for 2020 PGA 
+us_pga_2020_hole = hole_level_data[hole_level_data['tournament id']==401219481]
+
+R1 = us_pga_2020_hole[us_pga_2020_hole['round']==1]
+R2 = us_pga_2020_hole[us_pga_2020_hole['round']==2]
+R3 = us_pga_2020_hole[us_pga_2020_hole['round']==3]
+R4 = us_pga_2020_hole[us_pga_2020_hole['round']==4]
+
+#Break out into rounds
+g1 = pd.DataFrame(R1.groupby(['player id'])['Strokes Above Par'].sum())
+g1 = g1.rename(columns={'Strokes Above Par': 'R1'})
+
+g2 = pd.DataFrame(R2.groupby(['player id'])['Strokes Above Par'].sum())
+g2 = g2.rename(columns={'Strokes Above Par': 'R2'})
+
+g3 = pd.DataFrame(R3.groupby(['player id'])['Strokes Above Par'].sum())
+g3 = g3.rename(columns={'Strokes Above Par': 'R3'})
+
+g4 = pd.DataFrame(R4.groupby(['player id'])['Strokes Above Par'].sum())
+g4 = g4.rename(columns={'Strokes Above Par': 'R4'})
+
+ 
+#Merge individual rounds
+merge_rounds = pd.merge(us_pga_2020, g1, left_on=['player id'], right_index=True)
+merge_rounds = pd.merge(merge_rounds, g2, left_on=['player id'], right_index=True)
+merge_rounds = pd.merge(merge_rounds, g3, left_on=['player id'], right_index=True)
+merge_rounds = pd.merge(merge_rounds, g4, left_on=['player id'], right_index=True)
+
+
+
+
 
 #Jon Rahm finished 6 under
 #3.78 strokes gained (better than tour average)
