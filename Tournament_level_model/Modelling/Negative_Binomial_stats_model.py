37d996d98f7fc97a233285765e502f98422b8764
Killian Farrell <killianfarrell2@hotmail.com>
2021-07-28 18:26:12 +0100

    Add new models

--- Tournament_level_model/Modelling/Negative_Binomial_stats_model.py
+++ Tournament_level_model/Modelling/Negative_Binomial_stats_model.py
@@ -5,7 +5,6 @@ import numpy as np
 
 import statsmodels.api as sm
 
-
 #Import Tournament data
 data_location = 'D:\\KF_Repo\\PGA_Golf\\Tournament_level_model\\Data_manipulation\\model_data.csv'
 data = pd.read_csv(data_location)
@@ -29,53 +28,62 @@ golfers["i"] = golfers.index
 # Add i column back to dataframe
 data = pd.merge(data, golfers, left_on="player", right_on="golfer", how="left")
 data = data.rename(columns={"i": "i_golfer"}).drop("golfer", 1)
+# Shift data by 18
+data['Round_Score_shift'] = data['Round_Score'] + 18
+
+data['Round_Score_shift'].mean()
+17.24735700359722
+data['Round_Score_shift'].std()*data['Round_Score_shift'].std()
+10.040361602362125
+
+# Under dispersion
+print(10.04/17.24) 
+0.5823665893271461
 
 #Split into training data with rough 80:20 split
 training_data = data[data['date'] <'2020-10-01']
 test_data = data[data['date'] >='2020-10-01']
 
 
-
-
 import statsmodels.formula.api as smf
 
-f_rev = 'Round_Score~C(i_golfer)'
-model_rev = smf.ols(formula=f_rev, data=training_data).fit()
-model_rev.summary()
+from patsy import dmatrices
 
-#Predicted values are summarry and then adding golfer coefficient
-# For golfer 0 just intercept is used
+f_nb = 'Round_Score_shift~C(i_golfer)'
 
-X_test_orig = test_data['i_golfer']
+# Alpha parameter can be used to specify how much mean varies from variance
+response, predictors = dmatrices(f_nb, training_data, return_type='dataframe')
+model_nb = sm.GLM(response, predictors,family=sm.families.NegativeBinomial(alpha=1)).fit()
+print(model_nb.summary())
+
+
 #Add constant
 X_test = test_data['i_golfer']
 X_test = sm.add_constant(X_test)
-y_true = test_data['Round_Score'].values
 
+#Get prediction - this is expected mean value
+ypred = model_nb.predict(X_test)
 
-ypred = model_rev.predict(X_test)
-#Round prediction to give predicted score
-ypred_round = ypred.round(0)
+y_true = test_data['Round_Score_shift'].values
 
 # Metrics
 from sklearn import metrics
 
-
 # Print results of MAE
-print(metrics.mean_absolute_error(y_true, ypred_round))
+print(metrics.mean_absolute_error(y_true, ypred))
 
 # Print results of MSE
-print(metrics.mean_squared_error(y_true, ypred_round))
+print(metrics.mean_squared_error(y_true, ypred))
 
 # Print results of RMSE
-print(np.sqrt(metrics.mean_squared_error(y_true, ypred_round)))
+print(np.sqrt(metrics.mean_squared_error(y_true, ypred)))
 
-# Poisson Regression Model slightly better for RMSE and MSE
 
-2.5374607447285777
-10.3331837894422
-3.2145269931114595
 
+#Poisson stats
+2.5523287911598462
+10.298705102508523
+3.209159563267075
 
 
 
--- Tournament_level_model/Modelling/Poisson_regression_stats_models.py
+++ Tournament_level_model/Modelling/Poisson_regression_stats_models.py
@@ -5,7 +5,6 @@ import numpy as np
 
 import statsmodels.api as sm
 
-
 #Import Tournament data
 data_location = 'D:\\KF_Repo\\PGA_Golf\\Tournament_level_model\\Data_manipulation\\model_data.csv'
 data = pd.read_csv(data_location)
@@ -29,56 +28,46 @@ golfers["i"] = golfers.index
 # Add i column back to dataframe
 data = pd.merge(data, golfers, left_on="player", right_on="golfer", how="left")
 data = data.rename(columns={"i": "i_golfer"}).drop("golfer", 1)
+# Shift data by 18
+data['Round_Score_shift'] = data['Round_Score'] + 18
+
+data['Round_Score'].min()
 
 #Split into training data with rough 80:20 split
 training_data = data[data['date'] <'2020-10-01']
 test_data = data[data['date'] >='2020-10-01']
 
 
-
-
 import statsmodels.formula.api as smf
 
-f_rev = 'Round_Score~C(i_golfer)'
-model_rev = smf.ols(formula=f_rev, data=training_data).fit()
-model_rev.summary()
+f_pois = 'Round_Score_shift~C(i_golfer)'
 
-#Predicted values are summarry and then adding golfer coefficient
-# For golfer 0 just intercept is used
+model_pois = smf.poisson(formula=f_pois, data=training_data).fit()
+model_pois.summary()
 
-X_test_orig = test_data['i_golfer']
+
 #Add constant
 X_test = test_data['i_golfer']
 X_test = sm.add_constant(X_test)
-y_true = test_data['Round_Score'].values
 
+#Get prediction - this is just the mean number of events expected - decimal
+ypred = model_pois.predict(X_test)
+y_true = test_data['Round_Score_shift'].values
 
-ypred = model_rev.predict(X_test)
-#Round prediction to give predicted score
-ypred_round = ypred.round(0)
 
 # Metrics
 from sklearn import metrics
 
 
 # Print results of MAE
-print(metrics.mean_absolute_error(y_true, ypred_round))
+print(metrics.mean_absolute_error(y_true, ypred))
 
 # Print results of MSE
-print(metrics.mean_squared_error(y_true, ypred_round))
+print(metrics.mean_squared_error(y_true, ypred))
 
 # Print results of RMSE
-print(np.sqrt(metrics.mean_squared_error(y_true, ypred_round)))
+print(np.sqrt(metrics.mean_squared_error(y_true, ypred)))
 
-# Poisson Regression Model slightly better for RMSE and MSE
-
-2.5374607447285777
-10.3331837894422
-3.2145269931114595
-
-
-
-
-
-
-
+2.5523287911598462
+10.298705102508523
+3.209159563267075
